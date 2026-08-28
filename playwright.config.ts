d0f6b2de9adfb7bcd7a1ceaacb9ca42ca4b154d1
Marcelo Otone Aguiar <marcelootone@gmail.com>
2026-08-28 17:06:21 -0300

    Executa servidor Laravel antes dos testes E2E

--- playwright.config.ts
+++ playwright.config.ts
@@ -79,10 +79,10 @@ export default defineConfig({
 
   /* Sobe o servidor Laravel automaticamente antes dos testes.
      reuseExistingServer evita conflito caso o Laragon/artisan já esteja servindo em 8000. */
-  webServer: {
+  /*webServer: {
     command: 'php artisan serve --host=127.0.0.1 --port=8000',
 	url: 'http://127.0.0.1:8000',
     reuseExistingServer: true,
     timeout: 120 * 1000,
-  },
+  },*/
 });
